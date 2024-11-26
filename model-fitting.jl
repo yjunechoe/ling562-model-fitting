1c6cf78ae24396e3d3e2b1b4d5a01f3055a1814b
yjunechoe <jchoe001@gmail.com>
2024-11-26 18:21:56 -0500

    add demo with pnc

--- model-fitting.jl
+++ model-fitting.jl
@@ -1,3 +1,4 @@
+# using MKL
 using Arrow
 using Random
 using Statistics
@@ -10,7 +11,7 @@ using MixedModelsMakie
 ###############################
 
 # Continuing from R script
-CYC_2022 = Arrow.Table("CYC_2022.arrow")
+CYC_2022 = Arrow.Table("CYC_2022.arrow");
 Table(CYC_2022)
 
 fm = @formula(
@@ -29,6 +30,7 @@ mod.objective
 coefplot(mod; show_intercept=false)
 caterpillar(mod)
 shrinkageplot(mod; ellipse=true)
+## Aside: https://www.youtube.com/watch?v=y6KpCEW88gQ
 
 # Bootstrapped confidence intervals
 mod_boot = parametricbootstrap(
@@ -61,17 +63,82 @@ fm_max = @formula(
 mod_max = fit(MixedModel, fm_max, CYC_2022, Bernoulli());
 dof(mod_max)
 
-# Diagnostics
+# Overfitting diagnostics
 issingular(mod_max)
 mod_max.rePCA
 mod.σρs
 MixedModels.likelihoodratiotest(mod, mod_max)
 
-##################################
-# Embrace (vs. tame) uncertainty #
-##################################
+###########################
+# Performance on PNC data #
+###########################
 
-# Counterfactuals
+PNC_ay = Arrow.Table("PNC_ay.arrow");
+PNC_ay_tbl = Table(PNC_ay)
+PNC_fm_max = @formula(
+  vheight ~ birthyear_z2 * allophone * gender + logdur_z2 + frequency_z2 +
+                (1 + allophone + logdur_z2 + frequency_z2 | participant) +
+                (1 + birthyear_z2 * gender + logdur_z2 | word)
+);
+
+# brrr
+PNC_mod_max = @time fit(MixedModel, PNC_fm_max, PNC_ay);
+PNC_mod_max
+dof(PNC_mod_max)
+issingular(PNC_mod_max)
+
+# Inspect random effects
+length(unique(PNC_ay.participant))
+length(unique(PNC_ay.word))
+shrinkageplot(PNC_mod_max, :participant; ellipse=true)
+shrinkageplot(PNC_mod_max, :word; ellipse=true)
+
+# Diagnose suspicious shrinkage
+## Function to get the "red dots" (unconditional fits)
+function uncond_raneftables(m::LinearMixedModel)
+  m_uncond = deepcopy(m);
+  m_uncond.θ = m.optsum.initial .* 1e4;
+  updateL!(PNC_mod_max_uncond)
+  raneftables(PNC_mod_max_uncond)
+end ;
+ranef_uncond = uncond_raneftables(PNC_mod_max);
+
+## words
+ranef_word = ranef_uncond.word
+ranef_word[sortperm(ranef_word.birthyear_z2, rev=true)] # Check in R!
+
+## participants
+ranef_participant = ranef_uncond.participant
+ranef_participant[sortperm(ranef_participant.logdur_z2)]
+ranef_participant[sortperm(ranef_participant.var"allophone: ay0", rev=true)]
+
+# Try with excluding outliers and simplifying formula
+PNC_filtered = filter(
+  row -> row.word != "RIFLES" && row.participant ∉ ["PH91-2-19", "PH80-2-08"],
+  Table(PNC_ay)
+);
+PNC_mod_simple = @time fit(
+  MixedModel,
+  @formula(
+    vheight ~ birthyear_z2 * allophone * gender + logdur_z2 + frequency_z2 +
+                          (1 + allophone | participant) +
+                  zerocorr(0 + logdur_z2 + frequency_z2 | participant) +
+                  zerocorr(1 + birthyear_z2 + logdur_z2 | word)
+  ),
+  PNC_filtered
+);
+PNC_mod_simple
+
+issingular(PNC_mod_simple)
+shrinkageplot(PNC_mod_simple, :participant; ellipse=true)
+shrinkageplot(PNC_mod_simple, :word; ellipse=true)
+shrinkageplot(PNC_mod_simple, :word; ellipse=true, cols = ["birthyear_z2", "logdur_z2"])
+
+###################
+# Counterfactuals #
+###################
+
+mod
 
 ## Models store the response vector 
 CYC_2022.Accuracy
@@ -118,6 +185,10 @@ ridgeplot(parametricbootstrap(
   MersenneTwister(42), 100, mod_half;
   optsum_overrides=(;ftol_rel=1e-8)
 ); show_intercept=false)
+
+#################
+# Miscellaneous #
+#################
 
 # Power analysis is just bootstrapping on **simulated data**
 ## - Specify the X (dataframe minus column for response var)
